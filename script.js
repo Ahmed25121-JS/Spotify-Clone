8879f162b7c772c6dbdf5f8142212d87c5f719aa
Ahmed Saeed <ahmedsaeedqazi25@gmail.com>
2025-06-02 17:44:55 +0500

    Spotify Clone Further responsiveness

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 console.log("lets write javascript");
+let currentSong= new Audio();
+let songs;
+
+
+function formatTime(seconds) {
+  // Calculate minutes and seconds
+  const mins = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+
+  // Add leading zeros (e.g., 5 becomes "05")
+  const formattedMins = mins < 10 ? `0${mins}` : mins;
+  const formattedSecs = secs < 10 ? `0${secs}` : secs;
+
+  return `${formattedMins}:${formattedSecs}`;
+}
+
+
 
 async function getSongs(params) {
   let a = await fetch("http://127.0.0.1:3000/songs/");
@@ -8,7 +25,7 @@ async function getSongs(params) {
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
   console.log(as);
-  let songs = [];
+let songs = [];
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
@@ -20,12 +37,32 @@ async function getSongs(params) {
   }
   return songs;
 }
+// http://127.0.0.1:3000/songs/(Look%20Out)%20She's%20America%20-%20Otis%20McDonald.mp3
 
+const PlayMusic = (track,pause=true)=>{
+  currentSong.src="/songs/" + track
+  if (!pause){
+    currentSong.play()
+   
+      play.src ='pause.svg' 
+  }
+      document.querySelector('.songinfo').innerHTML =decodeURI(track )
+      
+      document.querySelector('.songtime').innerHTML ="00/00"
+}
+
+
+
+
+// Get the list of all songs
 async function main(params) {
-  // Get the list of all songs
-  let songs = await getSongs();
-  console.log(songs);
+  
 
+ songs = await getSongs();
+  console.log(songs);
+  PlayMusic(songs[0],true)
+
+  // Shows all the songs in the playlist
   let songUl = document
     .querySelector(".songList")
     .getElementsByTagName("ul")[0];
@@ -37,25 +74,110 @@ async function main(params) {
               <li>
                 <img class="invert" src="music.svg" alt="" />
                 <div class="info">
-                  <div>${song.replaceAll("%20", " ").replaceAll("(", "").replaceAll(")", "")}</div>
+                  <div>${song
+                    .replaceAll("%20", " ")
+                   }</div>
                   <div>Ahmed</div>
                 </div>
                 <div class="playnow">
                  <span>Play now</span> <img class="invert" src="play.svg" alt="" />
                 </div>
-              </li>`;
+              </li>`
+  }
+  // Attach an event listener to each song
+  // After using getelementby tagsname we get an array of objects (or you can call it an array like object)
+  // so Array.from is used to convert it into an actual array so we can use array methods on it
+ // Inside your main() function, after setting songUl.innerHTML:
+
+// Add a small delay to allow DOM to update
+ Array.from(
+    document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+    e.addEventListener("click", element=>{
+console.log(e.querySelector(".info").firstElementChild.innerHTML)
+PlayMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+    })
+  
+  })
+
+// Attach an event listener to previos,play,next
+// We can access id's directly we dont have to writedocument.getelementbyid
+// ids are global variables
+play.addEventListener('click',()=>{
+  if (currentSong.paused) {
+    currentSong.play()
+    play.src ='pause.svg'
+  } else {
+    currentSong.pause()
+    play.src="play.svg"
+  }
+})
+
+
+// listen for timeuodate function 
+currentSong.addEventListener("timeupdate",()=>{
+  console.log(currentSong.currentTime,currentSong.duration)
+  document.querySelector(".songtime").innerHTML =`
+  ${formatTime(currentSong.currentTime)}/${formatTime(currentSong.duration)}
+  `
+  document.querySelector('.circle').style.left=(currentSong.currentTime/currentSong.duration) *100 + "%"
+})
+
+// Add an event listener to seekbar
+document.querySelector(".seekbar").addEventListener('click',(e)=>{
+let percent = (e.offsetX/e.target.getBoundingClientRect().width)*100 
+console.log(percent)
+document.querySelector('.circle').style.left = percent  + "%"
+currentSong.currentTime=( currentSong.duration*percent) /100
+})
+
+
+// Add an event listener for hamburger
+document.querySelector(".hamburger").addEventListener('click',()=>{
+        document.querySelector('.left').style.left ="0" })
+
+// Add an event listener for clising hamburger
+document.querySelector(".close").addEventListener('click',()=>{
+   document.querySelector('.left').style.left ="-110%" 
+   
+})
+
+// add an evet listener to previous and next
+previous.addEventListener('click',(e)=>{
+  console.log("previos")
+  let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
+  console.log(songs,index)
+  if (index-1 >= 0) {
+    
+ PlayMusic(songs[index-1])
+  }
+})
+
+// add an event listener for next
+
+next.addEventListener('click',(e)=>{
+  console.log("next")
+  console.log(currentSong.src)
+
+  console.log(currentSong.src.split("/").slice(-1)[0])
+  let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
+  console.log(songs,index)
+  if ((index+1) > songs.length) {
+    
+ PlayMusic(songs[index+1])
   }
 
-  // Play the first song
-  var audio = new Audio(songs[0]);
-  audio.play();
+})
 
-  audio.addEventListener("loadeddata", () => {
-    let duration = audio.duration;
-    console.log(audio.duration, audio.currentSrc, audio.currentTime);
 
-    // The duration variable now holds the duration (in seconds) of the audio clip
-  });
+
+
+
+
 }
 
+
+
+
+
 main();
+
