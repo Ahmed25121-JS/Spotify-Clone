6a79bca186e40a690c2798cee0d5b2b39e5dcd5d
Ahmed Saeed <ahmedsaeedqazi25@gmail.com>
2025-06-04 20:50:24 +0500

    Dynamic Albums

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ async function getSongs(folder) {
   let div = document.createElement("div");
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
-  console.log(as);
-let songs = [];
+  // console.log(as);
+ songs = [];
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
@@ -37,37 +37,11 @@ let songs = [];
       songs.push(element.href.split(`/${folder}/`)[1]);
     }
   }
-  return songs;
-}
-// http://127.0.0.1:3000/songs/(Look%20Out)%20She's%20America%20-%20Otis%20McDonald.mp3
-
-const PlayMusic = (track,pause=true)=>{
-  currentSong.src=`/${currfolder}/` + track
-  if (!pause){
-    currentSong.play()
-   
-      play.src ='pause.svg' 
-  }
-      document.querySelector('.songinfo').innerHTML =decodeURI(track )
-      
-      document.querySelector('.songtime').innerHTML ="00/00"
-}
-
-
-
-
-// Get the list of all songs
-async function main(params) {
-  
-
- songs = await getSongs('songs/cs');
-  console.log(songs);
-  PlayMusic(songs[0],true)
-
   // Shows all the songs in the playlist
   let songUl = document
     .querySelector(".songList")
     .getElementsByTagName("ul")[0];
+    songUl.innerHTML=""
   console.log(songUl);
   for (const song of songs) {
     songUl.innerHTML =
@@ -86,11 +60,7 @@ async function main(params) {
                 </div>
               </li>`
   }
-  // Attach an event listener to each song
-  // After using getelementby tagsname we get an array of objects (or you can call it an array like object)
-  // so Array.from is used to convert it into an actual array so we can use array methods on it
- // Inside your main() function, after setting songUl.innerHTML:
-
+  
 // Creating an array and then passing in the song to play music
  Array.from(
     document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
@@ -100,6 +70,107 @@ PlayMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
     })
   
   })
+}
+// http://127.0.0.1:3000/songs/(Look%20Out)%20She's%20America%20-%20Otis%20McDonald.mp3
+
+const PlayMusic = (track,pause=true)=>{
+  currentSong.src=`/${currfolder}/` + track
+  if (!pause){
+    currentSong.play()
+   
+      play.src ='pause.svg' 
+  }
+      document.querySelector('.songinfo').innerHTML =decodeURI(track )
+      
+      document.querySelector('.songtime').innerHTML ="00/00"
+}
+
+// Display all the albums on the page
+async function displayAlbums(params) {
+  let a = await fetch(`http://127.0.0.1:3000/songs/`);
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  console.log(div);
+ let anchors= div.getElementsByTagName('a')
+
+let cardContainer = document.querySelector('.cardContainer')
+ let array =Array.from(anchors)
+  for (let index = 0; index < array.length; index++) {
+    const e = array[index];
+if (e.href.includes("/songs")) {
+let folder =e.href.split('/').slice(-2)[0]
+  // Get the metadat of the folders
+    let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
+  let response = await a.json();
+  cardContainer.innerHTML = cardContainer.innerHTML + `
+   <div data-folder="cs" class="card">
+              <div class="play">
+                <div class="play-button-circle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                    fill="none"
+                  >
+                    <path
+                      d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z"
+                      stroke="currentColor"
+                      stroke-width="1.5"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <img
+                src="/songs/${folder}/cover.jpg"
+                alt=""
+              />
+              <h2>${response.title}</h2>
+              <p>${response.description}</p>
+            </div>
+  
+  
+  `
+  
+}
+ }
+
+ 
+Array.from(document.getElementsByClassName('card')).forEach((card) => {
+  card.addEventListener('click', async (event) => {
+    // ✅ Use currentTarget to always get the .card element
+    const folder = event.currentTarget.dataset.folder;
+    console.log(folder);
+    
+    if (!folder) {
+      console.error("No data-folder attribute found on card");
+      return;
+    }
+    
+    songs = await getSongs(`songs/${folder}`);
+     // Check if songs are fetched correctly
+  });
+});
+  
+}
+displayAlbums()
+
+
+
+
+
+async function main(params) {
+  
+// Get the list of all the songs
+ await getSongs(`songs/ncs`);
+  console.log(songs);
+  PlayMusic(songs[0],true)
+
+
+
+
 
 // Attach an event listener to previos,play,next
 // We can access id's directly we dont have to writedocument.getelementbyid
@@ -183,6 +254,7 @@ document.querySelector(".range").getElementsByTagName('input')[0].addEventListen
 })
 
 // Load the playlist whenver card is clicked
+
 
 
 
